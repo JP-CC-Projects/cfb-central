06e275e4d8395e711f9feec28928b08bfa913d8c
JP-CC-Projects <joeparkcc@gmail.com>
2024-01-27 11:52:10 -0600

    resolved merge conflicts

--- cfb-project-vite/src/utils/setupAxiosInterceptor.ts
+++ cfb-project-vite/src/utils/setupAxiosInterceptor.ts
@@ -1,10 +1,7 @@
 import axios from './axiosClient'; 
 import store from '../redux/store';
 import { LOGIN_SUCCESS, LOGOUT } from '../redux/actions/authActions';
-<<<<<<< HEAD
-=======
 import refreshToken from '../services/authService';
->>>>>>> dev
 
 const setupAxiosInterceptor = () => {
   axios.interceptors.response.use(
@@ -15,27 +12,17 @@ const setupAxiosInterceptor = () => {
         originalRequest._retry = true;
         try {
           // Refresh token logic here
-<<<<<<< HEAD
-          const newAccessToken = await refreshToken(); // Implement this function
-=======
+
           const newAccessToken = await refreshToken(); 
 
           // Update local storage with the new access token
           localStorage.setItem('accessToken', newAccessToken);
 
           // Update Redux store (optional, depends on how you manage state)
->>>>>>> dev
           store.dispatch({
             type: LOGIN_SUCCESS,
             payload: { accessToken: newAccessToken }
           });
-<<<<<<< HEAD
-          originalRequest.headers['Authorization'] = `Bearer ${newAccessToken}`;
-          return axios(originalRequest);
-        } catch (refreshError) {
-          store.dispatch({ type: LOGOUT });
-          // Redirect to login or handle error
-=======
 
           // Update the authorization header with the new token
           originalRequest.headers['Authorization'] = `Bearer ${newAccessToken}`;
@@ -43,7 +30,6 @@ const setupAxiosInterceptor = () => {
         } catch (refreshError) {
           // Handle refresh token errors
           store.dispatch({ type: LOGOUT });
->>>>>>> dev
           return Promise.reject(refreshError);
         }
       }
