ebbe8185768e7d1aff2b849a31f70fbff5681b82
JP-CC-Projects <joeparkcc@gmail.com>
2024-04-07 04:36:36 -0500

    removed unused variable for build

--- cfb-project-vite/src/components/pages/map/MapPage.tsx
+++ cfb-project-vite/src/components/pages/map/MapPage.tsx
@@ -6,11 +6,16 @@ const MapPage = () => {
     const { teamId } = useParams<{ teamId?: string }>();
     // Default to 0 (or another appropriate default value) if teamId is undefined
     const parsedTeamId = teamId ? parseInt(teamId, 10) : 0;
-
+    // Date-related Constants
+    const currentDate = new Date();
+    const currentMonth = currentDate.getMonth();
+    const currentYear = currentDate.getFullYear();
+    // Will list 2023 Season Stats until August:
+    const teamSeason = currentMonth < 8 ? currentYear - 1 : currentYear;
     return (
-            <div>
-                <MapComponent teamId={parsedTeamId} teamSeason={2023} />
-            </div>
+        <div>
+            <MapComponent teamId={parsedTeamId} teamSeason={teamSeason} />
+        </div>
     );
 };
 
--- cfb-project-vite/src/components/pages/team/TeamPage.tsx
+++ cfb-project-vite/src/components/pages/team/TeamPage.tsx
@@ -4,21 +4,11 @@ import { useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchTeamDetails } from '../../../redux/actions/teamActions';
 import { AppDispatch, RootState } from '../../../redux/store';
-import TeamTimelinePage from './team-pages/team-page-timeline/TeamTimeLinePage';
-import TeamRosterPage from './team-pages/team-page-roster/TeamRosterPage';
 import TeamOverviewPage from './team-pages/team-overview/TeamOverviewPage';
-import MapComponent from '../map/MapComponent';
-import ChartComponent from './team-pages/team-page-chart/ChartComponent';
 import 'react-tabs/style/react-tabs.css';
 import './TeamPage.css';
 
 const TeamPage = () => {
-  // Date-related Constants
-  const currentDate = new Date();
-  const currentMonth = currentDate.getMonth();
-  const currentYear = currentDate.getFullYear();
-  // Will list 2023 Season Stats until August:
-  const teamSeason = currentMonth < 8 ? currentYear - 1 : currentYear;
 
   // Redux-related Constants
   const { teamId } = useParams<{ teamId?: string }>();
